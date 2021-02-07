2ddc37523a098e6aa960579f8b5b2704af57003b
Mahdi Lamb <mahdilamb@gmail.com>
2021-02-07 20:15:12 +0000

    Initial commit

---
+++ src/main/java/net/mahdilamb/charts/dataframe/DataSeriesCastException.java
@@ -0,0 +1,7 @@
+package net.mahdilamb.charts.dataframe;
+
+/**
+ * Exception thrown when a series is cast to the wrong type
+ */
+public class DataSeriesCastException extends ClassCastException {
+}
